568291f8744a51479afe4e0ece9da4f28b57b789
AyalewGM <ayalew.mersha@gmail.com>
2026-09-14 07:43:26 -0400

    F-008: register learner workspace API

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.parent_api import router as parent_router
 from app.parent_settings_web import router as parent_settings_web_router
 from app.parent_web import router as parent_web_router
 from app.services.llm_bootstrap import configure_tutor_engine
+from app.workspace_api import router as learner_workspace_router
 
 configure_tutor_engine()
 
@@ -20,6 +21,7 @@ app.include_router(adaptive_response_router, prefix=settings.api_prefix)
 app.include_router(diagnostic_router, prefix=settings.api_prefix)
 app.include_router(hint_router, prefix=settings.api_prefix)
 app.include_router(parent_router, prefix=settings.api_prefix)
+app.include_router(learner_workspace_router, prefix=settings.api_prefix)
 app.include_router(parent_web_router)
 app.include_router(parent_settings_web_router)
 
