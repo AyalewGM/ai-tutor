891ea20e76b1bccab2c00e90b3b171d1cac68f5c
AyalewGM <ayalew.mersha@gmail.com>
2026-09-17 17:42:41 -0400

    F-017 wire trusted session identity middleware

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.adaptive_api import router as adaptive_tutor_router
 from app.adaptive_response_api import router as adaptive_response_router
 from app.api import router as tutor_router
+from app.auth import session_identity_middleware
 from app.core.settings import settings
 from app.diagnostic_api import router as diagnostic_router
 from app.hint_api import router as hint_router
@@ -17,6 +18,7 @@ from app.workspace_api import router as learner_workspace_router
 configure_tutor_engine()
 
 app = FastAPI(title=settings.app_name, version="0.1.0")
+app.middleware("http")(session_identity_middleware)
 app.include_router(tutor_router, prefix=settings.api_prefix)
 app.include_router(adaptive_tutor_router, prefix=settings.api_prefix)
 app.include_router(adaptive_response_router, prefix=settings.api_prefix)
